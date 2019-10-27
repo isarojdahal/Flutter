57602f4d05069f0cd7f845d1dffd96f1eb663c2f
originalsaroj <sarojdahal1213@gmail.com>
2019-10-27 11:29:31 +0545

    model for users created

---
+++ json_reader/lib/models/users_model.dart
@@ -0,0 +1,14 @@
+class Users{
+
+   String picture;
+   String about;
+   String name;
+   String gender;
+   int age;
+   String email;
+   String phone;
+
+   Users(this.picture,this.about,this.name,this.gender,this.age,this.email,this.phone);
+
+    
+  }
